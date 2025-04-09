23b5779dbcf5437ce140dac0fbde84ea6db17c80
cat-ninja007 <150651906+cat-ninja007@users.noreply.github.com>
2025-04-09 18:11:17 +0800

    Update homepage and progress on content page

--- src/app/content/page.tsx
+++ src/app/content/page.tsx
@@ -1,10 +1,49 @@
-import Link from 'next/link';
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+
+import RedBackground from "@/assets/images/bg_front_cover.png";
+import Curtain from "@/assets/images/curtain.png";
+import DiamondPattern from "@/assets/images/bg_diamond_pattern.png";
+import GoldenClock from "@/assets/images/golden_clock.png";
+import Frame from "@/assets/images/frame.png";
 
 const ContentPage = () => {
   return (
     <>
-      <h1>This is Content Page</h1>
+      <div className="relative">
+        <div className="h-screen relative w-screen z-0">
+          <Image
+            src={RedBackground}
+            alt="RedBackground"
+            layout="fill"
+            objectFit="cover"
+          />
+          <div className="h-screen relative w-screen z-10 flex items-center">
+            <Image
+              src={DiamondPattern}
+              alt="DiamondPattern"
+              layout="fill"
+              objectFit="cover"
+            />
+            <div className="title absolute top-[10%] left-[20%]">
+              <h1 className="uppercase text-7xl">Content</h1>
+            </div>
+            <div className="card-container border-2 border-white h-[80%] flex items-center">
+              <Link href="/about" className="relative left-[10%] cursor hover:transform hover:-translate-y-7 transition-transform duration-300 ease-in-out">
+                <div className="w-64 h-96 border-2 bg-yellow-500"></div>
+              </Link>
+              <Link href="/about" className="relative -left-[5%] top-[10%] cursor hover:transform hover:-translate-y-7 transition-transform duration-300 ease-in-out">
+                <div className="w-64 h-96 border-2 bg-blue-500 rotate-8"></div>
+              </Link>
+              <Link href="/about" className="relative -left-[15%] top-[20%] cursor hover:transform hover:-translate-y-7 transition-transform duration-300 ease-in-out">
+                <div className="w-64 h-96 border-2 bg-green-500  rotate-14"></div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <h1>This is Content Page</h1>
       <Link href="/about">
         <div className="w-[64px] h-[64px] bg-red-600"></div>'
       </Link>
@@ -13,9 +52,9 @@ const ContentPage = () => {
       </Link>
       <Link href="/character">
         <div className="w-[64px] h-[64px] bg-purple-600"></div>'
-      </Link>
+      </Link> */}
     </>
-  )
-}
+  );
+};
 
 export default ContentPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
-import WelcomeBackground from "@/assets/images/homepage.png";
 import RedBackground from "@/assets/images/bg_front_cover.png";
 import Curtain from "@/assets/images/curtain.png";
 import DiamondPattern from "@/assets/images/bg_diamond_pattern.png";
 import GoldenClock from "@/assets/images/golden_clock.png";
 import Frame from "@/assets/images/frame.png";
-import HomepageText from "./components/HomepageText";
+import Cards from "@/assets/images/cards.png";
 
 const Home = () => {
   return (
@@ -44,12 +43,18 @@ const Home = () => {
               <p className="text-center text-3xl text-[#bf9b6a]">portfolio</p>
             </div>
             <div className="relative z-10 -top-[54%] ">
+              <div className="absolute z-10 w-[25%] rotate-180 -top-[80%] left-[5%]">
+                <Image src={Cards} alt="Cards" />
+              </div>
+              <div className="absolute z-10 w-[25%] -top-[80%] left-[72%]">
+                <Image src={Cards} alt="Cards" />
+              </div>
               <p className="text-center text-7xl text-[#bf9b6a]">
                 Between Crown
               </p>
               <p className="text-center text-7xl text-[#bf9b6a]">& Heart</p>
               <div className="flex justify-center mt-6">
-                <Link href="/content" >
+                <Link href="/content">
                   <div className="w-12 h-12 hover:bg-red-400 hover:mask-b-from-10% rounded-full cursor"></div>
                 </Link>
               </div>
@@ -57,19 +62,6 @@ const Home = () => {
           </div>
         </div>
       </div>
-      {/* <h3 className="absolute text-center text-white text-4xl top-[56%] left-[20%]
-        lg:text-5xl lg:top-[50%] lg:left-[48%]
-        "
-        >portfolio</h3> */}
-      {/* <div className="absolute top-[60%] left-[38%] pb-20">
-          <h2 className="relative text-center text-white font-bold text-7xl ">Between Crown <br/> & Heart</h2>
-          <Link href="/content">
-            <div className="relative w-20 h-24 mt-10 left-[40%]"></div>
-          </Link>
-        </div> */}
-      {/* <Link href="/content">
-          <div className="w-20 h-20 border-white border-4 absolute top-[79%] left-[51%] cursor"></div>'
-        </Link> */}
     </>
   );
 };
